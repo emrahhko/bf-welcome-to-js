3e2bde3d6e1852f67021812b66d58fda153075d1
emrahhko <emrah.hac@gmail.com>
2024-02-14 17:39:22 +0100

    advanced on the exercises

--- 1-what-is-programming/4-data-in-data-out/exercises/remove-spaces.js
+++ 1-what-is-programming/4-data-in-data-out/exercises/remove-spaces.js
@@ -4,38 +4,58 @@
 
   Data In:
 
-
   Data Out:
-
 
   Test Cases:
 
-
 */
+
+debugger;
 
 /* --- gather user input --- */
 
-let input = null;
-while (input === null) {
-  input = prompt('enter some text, all the spaces will be removed');
-  console.log(input);
-}
+// let input = null;
+// while (input === null) {
+//   input = prompt('enter some text, all the spaces will be removed');
+//   console.log(input);
+// }
 
 /* --- create new data with no spaces --- */
 
-let spaceless = '';
-for (let character of input) {
-  if (character !== ' ') {
-    spaceless = spaceless + character;
-    console.log(spaceless);
-  }
-}
+// let spaceless = '';
+// for (let character of input) {
+//   if (character !== ' ') {
+//     spaceless = spaceless + character;
+//     console.log(spaceless);
+//   }
+// }
 
 /* --- create a final message --- */
 
-let message = 'before: ' + input + '\nafter: ' + spaceless;
-console.log(message);
+// let message = 'before: ' + input + '\nafter: ' + spaceless;
+// console.log(message);
 
 /* --- display message to the user --- */
 
-alert(message);
+// alert(message);
+
+// My code
+
+let message = null;
+while (message === null) {
+  message = prompt('I will eliminate the space from your text');
+  console.log(message);
+}
+
+let noSpace = '';
+for (let character of message) {
+  if (character !== ' ') {
+    noSpace = noSpace + character;
+    console.log(noSpace);
+  }
+}
+
+let msg = 'before: ' + message + '\nafter: ' + noSpace;
+console.log(msg);
+
+alert(msg);
